097d27b53af695e173e82e94ecb856f508d788ba
ASUS-PC <rizkiafriani20@gmail.com>
2024-09-08 21:16:29 +0700

    add Mengembalikan Fungsi readline.kt

--- src/main/kotlin/Mengembalikan Fungsi readline.kt
+++ src/main/kotlin/Mengembalikan Fungsi readline.kt
@@ -1,11 +1,13 @@
 fun main(args: Array<String>) {
-    println("Enter a number and I'II double it: ")
+    //Doubler program parsing
+    println("Enter a number and I'll double it: ")
     val input = readLine()!!
-    var a = input.toInt()
+    var a = input.toInt() // eventually Double
+    a = a * 2
     println(a)
 
-    println("Enter a number, and I'II squer it :")
+    println("Enter a number, and I'll square it:")
     val number = readLine()!!.toInt()
     val square = number * number
-    println("Result: + square")
+    println("Result: " + square)
 }
